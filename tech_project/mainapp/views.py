73e1c888c0438af3ac4de5f7a7eee80108a6ed79
sturdevantj <58966417+sturdevant-j@users.noreply.github.com>
2020-04-27 18:09:56 -0700

    Hotel Project

---
+++ tech_project/mainapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def home (request):
+    products = ["Cherries", "Apples", "Oranges", "Strawberries", "Pears", "Watermelons",]
+    context = (
+        {
+            'products': products,
+        }
+    )
+    return render(request, "home.html", context)
+
+
+
